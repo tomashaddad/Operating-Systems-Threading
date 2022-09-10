00bf9c6883d63c85d7036b69f8ceacbab8da0c33
Tomas Haddad <tomashaddad@gmail.com>
2022-09-10 15:11:45 +1000

    Add graceful exit for Task 1

--- src/task1/main.cpp
+++ src/task1/main.cpp
@@ -1,12 +1,28 @@
+#include <unistd.h>
+
+#include <csignal>
 #include <filesystem>
 #include <iostream>
 
 #include "../common/Profiler.h"
+#include "../common/constants.h"
 #include "../common/print.h"
 #include "../common/time.h"
 #include "Task1.h"
 
+void signalHandler(int signum) {
+    std::cerr << utility::timestamp() << "The program has exceeded the maximum time limit of "
+              << constants::TIME_LIMIT << " seconds." << std::endl;
+    std::cerr << utility::timestamp() << "Terminating ..." << std::endl;
+    exit(signum);
+}
+
 int main(int argc, char **argv) {
+    std::cout << utility::timestamp() << "Task 1 has begun executing." << std::endl;
+
+    alarm(constants::TIME_LIMIT);
+    signal(SIGALRM, signalHandler);
+
     if (argc != 3) {
         std::cout << utility::timestamp() << "Two arguments are required, but " << argc - 1
                   << " were provided." << std::endl;
@@ -27,7 +43,8 @@ int main(int argc, char **argv) {
 
     profiler.stop();
 
-    std::cout << profiler.getDuration() << std::endl;
+    std::cout << utility::timestamp() << "Program executed in " << profiler.getDuration() << " ms."
+              << std::endl;
 
     return EXIT_SUCCESS;
 }
