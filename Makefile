7867b63b94a24ccf8c1d9652e1ce79d0aecc5ba3
Tomas Haddad <tomashaddad@gmail.com>
2022-09-11 15:00:29 +1000

    Set up Task 4 directory and Makefile

--- Makefile
+++ Makefile
@@ -15,9 +15,6 @@ OBJ_PATHS := \
 	obj/task4 \
 	obj/task5
 
-TARGET_NAME := a1
-TARGET_PATH := $(BIN_PATH)/$(TARGET_NAME)
-
 COMMON_DIR := $(SRC_PATH)/common
 TASK1_DIR := $(SRC_PATH)/task1
 TASK2_DIR := $(SRC_PATH)/task2
@@ -31,40 +28,46 @@ TASK3_TARGET := $(BIN_PATH)/task3
 TASK4_TARGET := $(BIN_PATH)/task4
 TASK5_TARGET := $(BIN_PATH)/task5
 
-SRC := $(foreach file, $(wildcard $(SRC_PATH)/*.cpp), $(notdir $(file)))
-OBJ := $(addprefix $(OBJ_PATH)/, $(SRC:.cpp=.o))
-
+# a callable function that returns cpp files in a directory
 list_cpp_in = $(foreach file, $(wildcard $(1)/*.cpp), $(notdir $(file)))
+# a callable function that transforms a list of cpp files into a list of obj files
 format_obj = $(addprefix $(OBJ_PATH)/$(1)/, $(2:.cpp=.o))
 
 TASK1_CPP = $(call list_cpp_in, $(TASK1_DIR))
 TASK2_CPP = $(call list_cpp_in, $(TASK2_DIR))
 TASK3_CPP = $(call list_cpp_in, $(TASK3_DIR))
+TASK4_CPP = $(call list_cpp_in, $(TASK4_DIR))
 COMMON_CPP = $(call list_cpp_in, $(COMMON_DIR))
 
 TASK1_OBJ := $(call format_obj,task1,$(TASK1_CPP))
 TASK2_OBJ := $(call format_obj,task2,$(TASK2_CPP))
 TASK3_OBJ := $(call format_obj,task3,$(TASK3_CPP))
+TASK4_OBJ := $(call format_obj,task4,$(TASK4_CPP))
 COMMON_OBJ := $(call format_obj,common,$(COMMON_CPP))
 
 makedir:
-	@mkdir -p $(BIN_PATH) $(OBJ_PATHS) $(DBG_PATH)
+	@mkdir -p $(BIN_PATH) $(OBJ_PATHS)
 
 task1: $(TASK1_TARGET)
 task2: $(TASK2_TARGET)
 task3: $(TASK3_TARGET)
 task4: $(TASK4_TARGET)
-task5: $(TASK5_TARGET)
+# task5: $(TASK5_TARGET)
 
 ######### LINKING TASKS
 
 $(TASK1_TARGET): $(COMMON_OBJ) $(TASK1_OBJ)
 	$(CC) -o $@ $^ $(CCFLAGS)
 
+# We don't want the main file of task1, so we remove it from the prereq lists
+
 $(TASK2_TARGET): $(COMMON_OBJ) $(filter-out obj/task1/main.o, $(TASK1_OBJ)) $(TASK2_OBJ)
 	$(CC) -o $@ $^ $(CCFLAGS)
 
 $(TASK3_TARGET): $(COMMON_OBJ) $(filter-out obj/task1/main.o, $(TASK1_OBJ)) $(TASK3_OBJ)
+	$(CC) -pthread -o $@ $^ $(CCFLAGS)
+
+$(TASK4_TARGET): $(COMMON_OBJ) $(filter-out obj/task1/main.o, $(TASK1_OBJ)) $(TASK4_OBJ)
 	$(CC) -pthread -o $@ $^ $(CCFLAGS)
 
 ######### COMPILING TASKS
@@ -76,6 +79,9 @@ $(OBJ_PATH)/task2/%.o: $(TASK2_DIR)/%.cpp
 	$(CC) $(CCOBJFLAGS) -o $@ $<
 
 $(OBJ_PATH)/task3/%.o: $(TASK3_DIR)/%.cpp
+	$(CC) -pthread $(CCOBJFLAGS) -o $@ $<
+
+$(OBJ_PATH)/task4/%.o: $(TASK4_DIR)/%.cpp
 	$(CC) -pthread $(CCOBJFLAGS) -o $@ $<
 
 $(OBJ_PATH)/common/%.o: $(COMMON_DIR)/%.cpp
---
+++ src/task4/main.cpp
@@ -0,0 +1,258 @@
+#include <fcntl.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <algorithm>
+#include <csignal>
+#include <cstring>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+#include "../common/Profiler.h"
+#include "../common/constants.h"
+#include "../common/print.h"
+#include "../common/readwrite.h"
+#include "../common/sort.h"
+#include "../common/time.h"
+#include "../task1/Task1.h"
+
+std::vector<std::string> globalWords;
+
+pthread_mutex_t g_mutexDescriptors;
+pthread_cond_t g_condFifosReady;
+int g_counter = 0;
+
+int alarmFired = 0;
+
+struct ThreadData {
+    int length;
+    std::vector<int>& indices;
+};
+
+struct ReduceData {
+    int length;
+    std::vector<std::string>& words;
+};
+
+void signalHandler(int signum) {
+    std::cerr << utility::timestamp() << "The program has exceeded the maximum time limit of "
+              << constants::TIME_LIMIT << " seconds." << std::endl;
+    std::cerr << utility::timestamp()
+              << "Threads will now destroy their resources, and the process will exit."
+              << std::endl;
+
+    std::cout << "Alarm: " << alarmFired << std::endl;
+
+    alarmFired = 1;
+    std::cout << "Alarm: " << alarmFired << std::endl;
+}
+
+void* sort(void* arg) {
+    Profiler profiler;
+    profiler.start();
+
+    ThreadData* data = (ThreadData*)arg;
+
+    auto start = std::begin(data->indices);
+    auto end = std::end(data->indices);
+
+    // Indices in the ThreadData struct is a reference, so this is done in-place
+    std::stable_sort(start, end, [](const int& a, const int& b) {
+        return globalWords[a].substr(2) < globalWords[b].substr(2);
+    });
+
+    std::string fifo = "src/task3/sorted/sorted_" + std::to_string(data->length);
+    const char* fifo_cstr = fifo.c_str();
+
+    mkfifo(fifo_cstr, 0666);
+    pthread_mutex_lock(&g_mutexDescriptors);
+    ++g_counter;
+    pthread_cond_signal(&g_condFifosReady);
+    pthread_mutex_unlock(&g_mutexDescriptors);
+
+    auto descriptor = open(fifo_cstr, O_WRONLY);
+    int counter = 0;
+    for (auto i : data->indices) {
+        if (alarmFired) {
+            std::cout << "Sort thread for words " << data->length << " signalled to stop."
+                      << std::endl;
+            break;
+        }
+        write(descriptor, globalWords[i].c_str(), globalWords[i].size());
+        counter++;
+    }
+
+    close(descriptor);
+
+    profiler.stop();
+
+    std::cout << utility::timestamp() << "Map3 sort for words of length " << data->length
+              << " took " << profiler.getDuration() << " ms" << std::endl;
+
+    return data;
+}
+
+void* map3(void* arg) {
+    std::cout << utility::timestamp() << "map3 thread started!" << std::endl;
+    // A mapping of word lengths to a vector of indices in the globalWords vector
+    std::map<int, std::vector<int>> indices;
+    pthread_t threads[constants::NUM_LENGTHS];
+
+    for (std::size_t i = 0; i < globalWords.size(); ++i) {
+        indices[globalWords[i].length()].push_back(i);
+    }
+
+    for (int length = constants::MIN_LENGTH; length <= constants::MAX_LENGTH; ++length) {
+        ThreadData* data = new ThreadData{length, indices[length]};
+        pthread_create(&threads[length - constants::MIN_LENGTH], NULL, &sort, data);
+    }
+
+    ThreadData* mapData;
+
+    for (int length = constants::MIN_LENGTH; length <= constants::MAX_LENGTH; ++length) {
+        pthread_join(threads[length - constants::MIN_LENGTH], (void**)&mapData);
+        delete mapData;
+    }
+
+    std::cout << utility::timestamp() << "Successfully joined all sort threads!" << std::endl;
+
+    return 0;
+}
+
+void* readFifo(void* arg) {
+    ReduceData* data = (ReduceData*)arg;
+
+    std::string fifo = "src/task3/sorted/sorted_" + std::to_string(data->length);
+    std::cout << utility::timestamp() << "Reading from " << fifo << std::endl;
+
+    auto descriptor = open(fifo.c_str(), O_RDONLY);
+
+    if (descriptor == -1) {
+        std::cout << utility::timestamp() << "Failed to create read only descriptor for FIFO "
+                  << fifo << std::endl;
+        return data;
+    }
+
+    char* word = new char[data->length];
+
+    while (true) {
+        if (read(descriptor, word, sizeof(char) * data->length)) {
+            if (alarmFired) {
+                std::cout << "Read thread for words " << data->length << " signalled to stop."
+                          << std::endl;
+                break;
+            }
+            data->words.push_back(std::string(word));
+        } else {
+            // Finished reading from FIFO
+            break;
+        }
+    }
+
+    close(descriptor);
+    delete word;
+
+    std::cout << utility::timestamp() << "Successfully read " << data->words.size()
+              << " words from " << fifo << std::endl;
+
+    return data;
+}
+
+void* reduce3(void* arg) {
+    std::cout << utility::timestamp() << "reduce3 thread started!" << std::endl;
+
+    pthread_mutex_lock(&g_mutexDescriptors);
+    while (g_counter < constants::NUM_LENGTHS) {
+        pthread_cond_wait(&g_condFifosReady, &g_mutexDescriptors);
+        std::cout << utility::timestamp() << "Total FIFOs created: " << g_counter << std::endl;
+    }
+    pthread_mutex_unlock(&g_mutexDescriptors);
+
+    // All FIFOS are ready, so we can start reading them
+
+    pthread_t threads[constants::NUM_LENGTHS];
+    std::vector<std::vector<std::string>> sortedLists(constants::NUM_LENGTHS);
+
+    for (int length = 3; length <= constants::MAX_LENGTH; ++length) {
+        ReduceData* data = new ReduceData{length, sortedLists[length - constants::MIN_LENGTH]};
+        pthread_create(&threads[length - constants::MIN_LENGTH], NULL, &readFifo, data);
+    }
+
+    ReduceData* reduceData;
+
+    for (int length = constants::MIN_LENGTH; length <= constants::MAX_LENGTH; ++length) {
+        pthread_join(threads[length - constants::MIN_LENGTH], (void**)&reduceData);
+        delete reduceData;
+    }
+
+    utility::mergeAndOutput(sortedLists, "src/task3/sorted/sorted.txt");
+
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    alarm(constants::TIME_LIMIT);
+    signal(SIGALRM, signalHandler);
+
+    if (argc != 3) {
+        std::cout << utility::timestamp() << "Two arguments are required, but " << argc - 1
+                  << " were provided." << std::endl;
+        utility::print_arguments(argc, argv);
+        return EXIT_FAILURE;
+    }
+
+    std::filesystem::path cwd = std::filesystem::current_path();
+
+    std::cout << utility::timestamp() << "The current working directory is: " << cwd << std::endl;
+
+    utility::print_arguments(argc, argv);
+
+    Profiler profiler;
+    profiler.start();
+
+    std::string inputDir(argv[1]);
+    std::string outputDir(argv[2]);
+
+    Task1::TaskFilter(inputDir, outputDir);
+
+    globalWords = utility::readFileIntoVector(outputDir);
+
+    pthread_t map, reduce;
+
+    pthread_mutex_init(&g_mutexDescriptors, NULL);
+
+    if (pthread_create(&map, NULL, &map3, NULL)) {
+        std::cout << utility::timestamp() << "Error creating map3 thread" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if (pthread_create(&reduce, NULL, &reduce3, NULL)) {
+        std::cout << utility::timestamp() << "Error creating reduce3 thread" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if (pthread_join(map, NULL)) {
+        std::cout << utility::timestamp() << "Error joining map3 thread" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if (pthread_join(reduce, NULL)) {
+        std::cout << utility::timestamp() << "Error joining reduce3 thread" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    pthread_mutex_destroy(&g_mutexDescriptors);
+
+    profiler.stop();
+
+    std::cout << utility::timestamp() << "Program executed in " << profiler.getDuration() << " ms."
+              << std::endl;
+
+    return EXIT_SUCCESS;
+}
