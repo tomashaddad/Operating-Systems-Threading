619773be00855e9a98c6f43db1f144c5dd5abb37
Tomas Haddad <tomashaddad@gmail.com>
2022-09-11 20:36:20 +1000

    Increase time limit constant to 20

--- src/common/constants.h
+++ src/common/constants.h
@@ -5,7 +5,7 @@ namespace constants {
 int constexpr MIN_LENGTH = 3;
 int constexpr MAX_LENGTH = 15;
 int constexpr NUM_LENGTHS = MAX_LENGTH - MIN_LENGTH + 1;
-int constexpr TIME_LIMIT = 10;
+int constexpr TIME_LIMIT = 20;
 }  // namespace constants
 
 #endif  // CONSTANTS_H
